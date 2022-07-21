9e00d9e5d008e648c59916117a0d2bdbcb513020
emmaru <littleoduozu@gmail.com>
2022-07-21 17:00:23 +0900

    [update] #9 Made Models: League, Team, TeamJapanese, Match

--- main/models.py
+++ main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class League(models.Model):
+    name = models.CharField(max_length=40)
+
+    class Meta:
+        db_table = 'league'
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=40)
+    crest = models.URLField(max_length=200)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'team'
+
+
+class TeamJapanese(models.Model):
+    team = models.OneToOneField(Team, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=40)
+
+    class Meta:
+        db_table = 'team_japanese'
+
+
+class Match(models.Model):
+    date = models.DateTimeField()
+    match_day = models.PositiveIntegerField()
+    home = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_matches')
+    away = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_matches')
+
+    class Meta:
+        db_table = 'match'

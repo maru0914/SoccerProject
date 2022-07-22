9aca19995a812e3d82ecdf654f2c76aba9d57f05
emmaru <littleoduozu@gmail.com>
2022-07-22 11:28:32 +0900

    [update] #9 Created Season model and added unique constraints

--- main/models.py
+++ main/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class League(models.Model):
-    name = models.CharField(max_length=40)
-    emblem = models.URLField(max_length=200)
+    name = models.CharField(max_length=40, unique=True)
+    emblem = models.URLField(max_length=200, null=True)
 
     class Meta:
         db_table = 'league'
@@ -19,12 +19,12 @@ class LeagueJapanese(models.Model):
 
 class Team(models.Model):
     name = models.CharField(max_length=40)
-    crest = models.URLField(max_length=200)
+    crest = models.URLField(max_length=200, null=True)
     league = models.ForeignKey(League, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'team'
-
+        constraints = [models.UniqueConstraint(fields=['name', 'league'], name='unique_team')]
 
 class TeamJapanese(models.Model):
     team = models.OneToOneField(Team, on_delete=models.CASCADE, primary_key=True)
@@ -34,11 +34,24 @@ class TeamJapanese(models.Model):
         db_table = 'team_japanese'
 
 
+class Season(models.Model):
+    year = models.PositiveIntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'season'
+        constraints = [models.UniqueConstraint(fields=['year', 'league'], name='unique_season')]
+
+
 class Match(models.Model):
     date = models.DateTimeField()
     match_day = models.PositiveIntegerField()
     home = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_matches')
     away = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_matches')
+    season = models.ForeignKey(Season, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'match'
+        constraints = [models.UniqueConstraint(fields=['match_day', 'home', 'away', 'season'], name='unique_match')]

02286e5755307892ada8f705e8d7f37ff7eb3ca1
emmaru <littleoduozu@gmail.com>
2022-07-24 18:06:37 +0900

    [update] #9 Implemented Japanese localization of team names

--- main/templatetags/localize_tags.py
+++ main/templatetags/localize_tags.py
@@ -1,6 +1,6 @@
 from django import template 
 
-from main.models import League
+from main.models import League, Team
 
 register = template.Library()
 
@@ -8,4 +8,10 @@ register = template.Library()
 def league_to_ja(name):
     league = League.objects.get(name=name)
     japanese_name = league.leaguejapanese.name
+    return japanese_name
+
+@register.filter
+def team_to_ja(name):
+    team = Team.objects.get(name=name)
+    japanese_name = team.teamjapanese.name
     return japanese_name

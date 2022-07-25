6de4fa7bd2a5b6c8b3155f8659db5eeb9efc7cbc
emmaru <littleoduozu@gmail.com>
2022-07-25 16:38:29 +0900

    [update] #16 Modified schedule page to process league and team

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from main.views import HomeView, LeagueScheduleView, TeamView
+from main.views import HomeView, ScheduleView
 
 app_name = 'main'
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
-    path('league_schedule/<str:league_code>', LeagueScheduleView.as_view(), name='league_schedule'),
-    path('team/<str:league_code>/<int:id>', TeamView.as_view(), name='team'),
+    path('schedule/<str:key>', ScheduleView.as_view(),name='schedule'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,6 @@
 from django.db.models import Q
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView
 
-from main import get_info
 from main.models import League, Match, Team
 
 
@@ -10,32 +9,29 @@ class HomeView(ListView):
     template_name = "main/home.html"
 
 
-class LeagueScheduleView(ListView):
+class ScheduleView(ListView):
     model = Match
     paginate_by = 10
-    template_name = "main/league_schedule.html"
+    template_name = "main/schedule.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        league_code = self.kwargs.get('league_code')
-        context['team_list'] = Team.objects.filter(league__code=league_code)
-        context['league'] = League.objects.get(code=league_code)
+        try:
+            team_id = int(self.kwargs.get('key'))
+            context['team'] = Team.objects.get(id=team_id)
+
+        except ValueError:
+            league_code = self.kwargs.get('key')
+            context['team_list'] = Team.objects.filter(league__code=league_code)
+            context['league'] = League.objects.get(code=league_code)
         return context
 
     def get_queryset(self, **kwargs):
         queryset = super().get_queryset(**kwargs)
-        league_code = self.kwargs.get('league_code')
-        queryset = queryset.filter(home__league__code=league_code)
+        try:
+            team_id = int(self.kwargs.get('key'))
+            queryset = queryset.filter(Q(home=team_id) | Q(away=team_id))
+        except ValueError:
+            league_code = self.kwargs.get('key')
+            queryset = queryset.filter(home__league__code=league_code)
         return queryset
-        
-
-class TeamView(TemplateView):
-    template_name = "main/team.html"
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        league_code = self.kwargs.get('league_code')
-        team_id = self.kwargs.get('id')
-        context['match_list'] = Match.objects.filter(Q(home=team_id) | Q(away=team_id))
-        context['league_code'] = league_code
-        context['team'] = Team.objects.get(id=team_id)
-        return context

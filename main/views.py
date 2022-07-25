68e88efd9306d8629f057f2753fbfa7cc1e69a6e
emmaru <littleoduozu@gmail.com>
2022-07-25 13:11:05 +0900

    [update] #16 Created league_schedule page

--- main/views.py
+++ main/views.py
@@ -10,15 +10,24 @@ class HomeView(ListView):
     template_name = "main/home.html"
 
 
-class LeagueView(TemplateView):
-    template_name = "main/league.html"
+class LeagueScheduleView(ListView):
+    model = Match
+    paginate_by = 10
+    template_name = "main/league_schedule.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         league_code = self.kwargs.get('league_code')
         context['team_list'] = Team.objects.filter(league__code=league_code)
-        context['league_code'] = league_code
+        context['league'] = League.objects.get(code=league_code)
         return context
 
+    def get_queryset(self, **kwargs):
+        queryset = super().get_queryset(**kwargs)
+        league_code = self.kwargs.get('league_code')
+        queryset = queryset.filter(home__league__code=league_code)
+        return queryset
+        
 
 class TeamView(TemplateView):
     template_name = "main/team.html"

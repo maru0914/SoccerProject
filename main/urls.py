48d946ffba46bcebc9327934769f4109745b6859
emmaru <littleoduozu@gmail.com>
2022-07-25 13:11:05 +0900

    [update] #16 Created league_schedule page

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from main.views import HomeView, LeagueView, TeamView
+from main.views import HomeView, LeagueScheduleView, TeamView
 
 app_name = 'main'
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
-    path('league/<str:league_code>', LeagueView.as_view(), name='league'),
+    path('league_schedule/<str:league_code>', LeagueScheduleView.as_view(), name='league_schedule'),
     path('team/<str:league_code>/<int:id>', TeamView.as_view(), name='team'),
 ]

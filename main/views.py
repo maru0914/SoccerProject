284f135c684f3f8c3c0c25e1f084e7c2e5713b3e
emmaru <littleoduozu@gmail.com>
2022-07-26 11:28:24 +0900

    [update] #20 Added a schedule page that summarizes all leagues

--- main/views.py
+++ main/views.py
@@ -16,23 +16,28 @@ class ScheduleView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        try:
-            team_id = int(self.kwargs.get('key'))
+        key = self.kwargs.get('key')
+        if key == 'all':
+            pass
+        elif key.isdigit():
+            team_id = int(key)
             context['team'] = Team.objects.get(id=team_id)
-
-        except ValueError:
-            league_code = self.kwargs.get('key')
+        else:
+            league_code = key
             context['team_list'] = Team.objects.filter(league__code=league_code)
             context['league'] = League.objects.get(code=league_code)
         return context
 
     def get_queryset(self, **kwargs):
         queryset = super().get_queryset(**kwargs)
-        try:
-            team_id = int(self.kwargs.get('key'))
+        key = self.kwargs.get('key')
+        if key == 'all':
+            pass
+        elif key.isdigit():
+            team_id = int(key)
             queryset = queryset.filter(Q(home=team_id) | Q(away=team_id))
-        except ValueError:
-            league_code = self.kwargs.get('key')
+        else:
+            league_code = key
             queryset = queryset.filter(home__league__code=league_code)
         return queryset
 

d0f1ac206e90fa45bedf626083efa1a9796cebd4
emmaru <littleoduozu@gmail.com>
2022-08-22 17:45:25 +0900

    [update] #34 Added score info to dashboard

--- mypage/views.py
+++ mypage/views.py
@@ -13,19 +13,20 @@ class DashboardView(TemplateView):
     """
     Display an dashboard for specific user.
 
-    **Template:**
-
-    :template:'mypage/dashboard.html'
-
     **Context:**
 
-    :context: {
-        'team': team,
-        'next_five_matches': next_five_matches,
-        'five_standings': five_standings,
-        'last_match': last_match,
-        'last_match_against': last_match_against
-    }
+    ``five_standings``
+        A list of five_standings info.
+    ``last_match``
+        A dict of last match info.
+    ``next_five_matches``
+        A list of next 5 matches info.
+    ``team``
+        An instance of :model:`main.Team`.
+
+    **Template:**
+
+    :template:`mypage/dashboard.html`
     """
     template_name = 'mypage/dashboard.html'
 
@@ -35,28 +36,46 @@ class DashboardView(TemplateView):
         team_id = 59
         team = Team.objects.get(id=team_id)
         context['team'] = team
-        print(type(team))
-        next_five_matches = []
 
+        next_five_matches = []
         coming_matches = Match.objects.filter(
             (Q(home=team_id) | Q(away=team_id)) & Q(date__gte=datetime.now()))
         for i, match in enumerate(coming_matches):
             match_info = {}
             match_info['date'] = match.date
             match_info['match_day'] = match.match_day
-            match_info['against'] = match.away.name if match.home.id == team_id else match.home.name
+            if match.home.id == team_id:
+                match_info['opponent'] = match.away.name
+            else:
+                match_info['opponent'] = match.home.name
             next_five_matches.append(match_info)
             if i == 4:
                 break
         context['next_five_matches'] = next_five_matches
         context['five_standings'] = self.get_five_standings(team)
-        last_match= Match.objects.filter(
+
+        last_match = {}
+        match= Match.objects.filter(
             (Q(home=team_id) | Q(away=team_id)) & Q(date__lte=datetime.now())).last()
-        context['last_match'] = last_match
-        if last_match.home.id == team_id:
-            context['last_match_against'] = last_match.away.name
+        last_match['date'] = match.date
+        if match.home.id == team_id:
+            last_match['opponent'] = match.away.name
+            team_score = match.home_score
+            opponent_score = match.away_score
         else:
-            context['last_match_against'] = last_match.home.name
+            last_match['opponent'] = match.home.name
+            team_score = match.away_score
+            opponent_score = match.home_score
+        if team_score > opponent_score:
+            last_match['result'] = 'win'
+        elif team_score < opponent_score:
+            last_match['result'] = 'lose'
+        else:
+            last_match['result'] = 'draw'
+        last_match['home_score'] = match.home_score
+        last_match['away_score'] = match.away_score
+
+        context['last_match'] = last_match
 
         return context
 

d7e62749848070681f536f2e417a03ede8e122a8
emmaru <littleoduozu@gmail.com>
2022-08-22 17:45:25 +0900

    [update] #34 Added score info to dashboard

--- main/models.py
+++ main/models.py
@@ -1,7 +1,12 @@
+"""Models for main app.
+"""
 from django.db import models
 
 
 class League(models.Model):
+    """
+    Stores a league info.
+    """
     code = models.CharField(max_length=40, unique=True)
     name = models.CharField(max_length=40)
     emblem = models.URLField(max_length=200, null=True)
@@ -11,6 +16,9 @@ class League(models.Model):
 
 
 class LeagueJapanese(models.Model):
+    """
+    Stores a league name in Japanese, related to :model:`main.League`.
+    """
     league = models.OneToOneField(League, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=40)
 
@@ -19,6 +27,9 @@ class LeagueJapanese(models.Model):
 
 
 class Team(models.Model):
+    """
+    Stores a team info, related to :model:`main.League`.
+    """
     name = models.CharField(max_length=40)
     crest = models.URLField(max_length=200, null=True)
     league = models.ForeignKey(League, on_delete=models.CASCADE)
@@ -28,6 +39,9 @@ class Team(models.Model):
         constraints = [models.UniqueConstraint(fields=['name', 'league'], name='unique_team')]
 
 class TeamJapanese(models.Model):
+    """
+    Stores a team name in Japanese, related to :model:`main.Team`.
+    """
     team = models.OneToOneField(Team, on_delete=models.CASCADE, primary_key=True)
     name = models.CharField(max_length=40)
 
@@ -36,6 +50,9 @@ class TeamJapanese(models.Model):
 
 
 class Season(models.Model):
+    """
+    Stores a season info, related to :model:`main.League`.
+    """
     year = models.PositiveIntegerField()
     start_date = models.DateField()
     end_date = models.DateField()
@@ -47,19 +64,28 @@ class Season(models.Model):
 
 
 class Match(models.Model):
+    """
+    Stores a match info, related to :model:`main.Team` and :model:`main.Season`.
+    """
     date = models.DateTimeField()
     match_day = models.PositiveIntegerField()
     home = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='home_matches')
     away = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='away_matches')
     season = models.ForeignKey(Season, on_delete=models.CASCADE)
+    home_score = models.PositiveIntegerField(null=True)
+    away_score = models.PositiveIntegerField(null=True)
 
     class Meta:
         db_table = 'match'
-        constraints = [models.UniqueConstraint(fields=['match_day', 'home', 'away', 'season'], name='unique_match')]
+        constraints = [models.UniqueConstraint(
+            fields=['match_day', 'home', 'away', 'season'], name='unique_match')]
         ordering = ['date']
 
 
 class Standing(models.Model):
+    """
+    Stores a standing info, related to :model:`main.Team` and :model:`main.League`.
+    """
     rank = models.PositiveIntegerField(null=True)
     point = models.PositiveIntegerField()
     played = models.PositiveIntegerField()

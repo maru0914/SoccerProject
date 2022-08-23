10c320ea0d8672bebb2c386db1e57e010638b8d3
emmaru <littleoduozu@gmail.com>
2022-08-23 13:57:17 +0900

    [update] #36 Created custom user model named 'MyUser'

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,14 @@
+"""forms setting for accounts app"""
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+
+from accounts.models import MyUser
 
 
 class RegisterUserForm(UserCreationForm):
-   
+    """A form for creating new users. Includes all the required fields, plus a repeated password."""
+
     class Meta:
-        model = User
+        model = MyUser
         fields = ('username', 'password1', 'password2')
 
     def __init__(self, *args, **kwargs):

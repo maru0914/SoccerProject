58e10cab713cd849e50c961b6d89c98d32f0c049
emmaru <littleoduozu@gmail.com>
2022-08-24 13:45:35 +0900

    [update] #36 Added select team page and corresponding view.

--- mypage/urls.py
+++ mypage/urls.py
@@ -2,10 +2,12 @@
 """
 from django.urls import path
 
-from mypage.views import DashboardView
+from mypage.views import DashboardView, SelectTeamView, register_team
 
 app_name = 'mypage'
 
 urlpatterns = [
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('select_team/', SelectTeamView.as_view(), name='select_team'),
+    path('register_team/', register_team, name='register_team'),
 ]

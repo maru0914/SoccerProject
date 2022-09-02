be75f7236948732e87e26bb236692d4a081244b3
emmaru <littleoduozu@gmail.com>
2022-09-02 20:39:59 +0900

    [fix] #54 fixed teamname highlight in standing table

--- static/js/script.js
+++ static/js/script.js
@@ -1,42 +1,50 @@
-$(document).ready(function() {
-    lightbox.option({
-        'wrapAround': true
-      })
+$(document).ready(function () {
+  lightbox.option({
+    'wrapAround': true
+  })
 
-      $('.nav-button').click(function() {
-        $('.nav-button').toggleClass('change');
-      });
-    
-      $(window).scroll(function() {
-        let position = $(this).scrollTop();
-        if(position >= 200) {
-          $('.nav-menu').addClass('custom-navbar');
-        } else {
-          $('.nav-menu').removeClass('custom-navbar');
-        }
-      });
-      $(window).scroll(function() {
-        let position = $(this).scrollTop();
-        
-        if(position >= 350) {
-            $('.gallery').addClass('change');
-        } else {
-            $('.gallery').removeClass('change');
-        }
-      });
-      $('[data-toggle="tooltip"]').tooltip();
-      
-      $('.gallery-list-item').click(function() {
-        let value = $(this).attr('data-filter');
-        if(value === 'all') {
-          $('.filter').show(300);
-        } else {
-          $('.filter').not('.' + value).hide(300);
-          $('.filter').filter('.' + value).show(300);
-        }
-      });
+  $('.nav-button').click(function () {
+    $('.nav-button').toggleClass('change');
+  });
 
-      $('.gallery-list-item').click(function() {
-        $(this).addClass('active-item').siblings().removeClass('active-item');
-      });
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    if (position >= 200) {
+      $('.nav-menu').addClass('custom-navbar');
+    } else {
+      $('.nav-menu').removeClass('custom-navbar');
+    }
+  });
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+
+    if (position >= 350) {
+      $('.gallery').addClass('change');
+    } else {
+      $('.gallery').removeClass('change');
+    }
+  });
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $('.gallery-list-item').click(function () {
+    let value = $(this).attr('data-filter');
+    if (value === 'all') {
+      $('.filter').show(300);
+    } else {
+      $('.filter').not('.' + value).hide(300);
+      $('.filter').filter('.' + value).show(300);
+    }
+  });
+
+  $('.gallery-list-item').click(function () {
+    $(this).addClass('active-item').siblings().removeClass('active-item');
+  });
+
+  $('tr').mouseover(function () {
+    $(this).children('td.teamname').addClass('teamname-hover');
+  });
+
+  $('tr').mouseout(function () {
+    $(this).children('td.teamname').removeClass('teamname-hover');
+  });
 });
